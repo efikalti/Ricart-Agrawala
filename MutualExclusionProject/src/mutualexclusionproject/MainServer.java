2ad62d66f3079aba244dd56b6f7a7828d14c30f6
kaltirim <kaltirim@lab.grid.auth.gr>
2015-02-09 15:12:23 +0200

    refactoring names and adding comments to main server classes

--- MutualExclusionProject/src/mutualexclusionproject/MainServer.java
+++ MutualExclusionProject/src/mutualexclusionproject/MainServer.java
@@ -8,7 +8,10 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 /**
- *
+ * Main server keeping track of every running process and answering to requests
+ * from processes.
+ * Works for 3 processes.
+ * Singleton class.
  * @author efi
  */
 public class MainServer {
@@ -18,9 +21,10 @@ public class MainServer {
     private ExecutorService     threadPool    = null;
     private ArrayList<Entry>    HostTable     = null;
     private int                 number        = 0;
+    private final int           numProcesses  = 3;
     
     private MainServer() {
-        this.threadPool = Executors.newFixedThreadPool(3);
+        this.threadPool = Executors.newFixedThreadPool(numProcesses);
     }
     
     public static MainServer getInstance() {
@@ -32,6 +36,10 @@ public class MainServer {
         private static final MainServer INSTANCE = new MainServer();
     }
     
+    /**
+     * Initialize the server and bind it to the specified port.
+     * @param port 
+     */
     public void Initialize(int port)
     {
         try {
@@ -43,6 +51,10 @@ public class MainServer {
         this.run();
     }
     
+    /**
+     * Accept incoming requests to the port, create a thread of MainServerWorker 
+     * to handle any incoming request and continue listening.
+     */
     public void run() {
         System.out.println("Main Server running...");
         while(!isStopped)
@@ -73,16 +85,29 @@ public class MainServer {
         }
     }
     
+    /**
+     * Call to retrieve the host table safely.
+     * @return the host table containing the entry of every process running at the time.
+     */
     public synchronized ArrayList<Entry> getProcesses ()
     {
         return this.HostTable;
     }
     
+    /**
+     * Call to assign a number to every process.
+     * @return an increment of the number starting from 1.Different for each process.
+     */
     public synchronized int getNumber()
     {
         return ++this.number;
     }
     
+    /**
+     * Call to add a new process in the host table.This is the method every new process
+     * calls before the pre-protocol.
+     * @param e 
+     */
     public synchronized void register (Entry e)
     {
         this.HostTable.add(e);
--- MutualExclusionProject/src/mutualexclusionproject/MainServerWorker.java
+++ MutualExclusionProject/src/mutualexclusionproject/MainServerWorker.java
@@ -8,21 +8,28 @@ import java.net.Socket;
 import java.util.ArrayList;
 
 /**
- *
+ * Class that handles the requests of every process calling the main server.
+ * For every process requests a new thread of this class is created to handle it.
  * @author efi
  */
 public final class MainServerWorker implements Runnable{
     
-    private final Socket            clientSocket;
+    private final Socket            ProcessSocket;
     private final PrintWriter       out;
     private final BufferedReader    in;
     private       ArrayList<Entry>  HostTable;
     
+    /**
+     * Save the process that did the request.
+     * Establish communication connection with the process.
+     * @param c
+     * @throws IOException 
+     */
     public MainServerWorker (Socket c) throws IOException
     {
-        this.clientSocket = c;
-        out = new PrintWriter(clientSocket.getOutputStream(), true);
-        in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        this.ProcessSocket = c;
+        out = new PrintWriter(ProcessSocket.getOutputStream(), true);
+        in = new BufferedReader(new InputStreamReader(ProcessSocket.getInputStream()));
     }
 
     @Override
@@ -30,15 +37,23 @@ public final class MainServerWorker implements Runnable{
         try {
             String inputLine;
             
+            //There are the programmed commands a process may ask from the server
+            //These are:
             OUTER:
             while ((inputLine = in.readLine()) != null) {
+                print(inputLine);
                 switch (inputLine) {
+                    //To end the conversation and terminate the connection
                     case "quit":
                         break OUTER;
+                    //If the process is new and needs to register in the host table
                     case "register":
+                        /*process sends its details(name,host,port) and the serverworker adds them to the 
+                        main server.
+                        This is achieved by calling the local method register that will contact the main server.
+                        */
                         if (!(inputLine = in.readLine()).equals("ok"))
                         {
-                            print(inputLine);
                             String parts[] = inputLine.split(",");
                             if (parts.length == 3)
                             {
@@ -56,8 +71,11 @@ public final class MainServerWorker implements Runnable{
                         }
                         out.flush();
                         break;
+                    //Process asks for the host table in order to send requests to the other processes.
                     case "send HostTable":
+                        //get the host table from the main server
                         this.HostTable = MainServer.getInstance().getProcesses();
+                        //send each process line by line
                         for (Entry t : this.HostTable)
                         {
                             out.println(t.toString());
@@ -69,8 +87,10 @@ public final class MainServerWorker implements Runnable{
                         break;
                 }
             }
+            //close communication connections
             out.close();
             in.close();
+            //print out host table for debugging reasons
             System.out.println("HostTable: ");
             for (Entry t : this.HostTable)
             {
@@ -82,11 +102,21 @@ public final class MainServerWorker implements Runnable{
         }
     }
     
+    /**
+     * Call to the main server instance to register a new process
+     * @param name
+     * @param hostname
+     * @param port 
+     */
     private synchronized void register (String name, String hostname, int port)
     {
         MainServer.getInstance().register(new Entry(name,hostname,port));
     }
     
+    /**
+     * Print the message of the client for debugging reasons
+     * @param str 
+     */
     public void print(String str)
     {
         System.out.println("Client says: " + str);
